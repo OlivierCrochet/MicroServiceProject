d002bb498a6179ec9b1f0d240941e425334c0309
Olivier <oliviercrochet1@gmail.com>
2019-12-03 14:17:10 +0100

    init

---
+++ src/main/java/fr/olivier/tp1/tp1ms/Tp1msApplication.java
@@ -0,0 +1,13 @@
+package fr.olivier.tp1.tp1ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1msApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1msApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/olivier/tp1/tp1ms/Tp1msApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.olivier.tp1.tp1ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp1msApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
